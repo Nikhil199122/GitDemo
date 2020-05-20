2a537cb19b738872ee411f3a21c47cb85325ac0e
Nikhil <nvshahane91@gmail.com>
2020-05-20 15:34:32 +0530

    This is commit from American TimeZone guy

---
+++ src/test/java/qaclickacademy/RESTAPI.java
@@ -0,0 +1,22 @@
+package qaclickacademy;
+
+import org.testng.annotations.Test;
+
+public class RESTAPI {
+	
+	
+	@Test
+	public void postJira()
+	{
+		System.out.println("PostJira");
+		
+		
+	}
+	
+	@Test
+	public void DeleteTwitter()
+	{
+		System.out.println("DeleteTwitter");
+	}
+
+}
